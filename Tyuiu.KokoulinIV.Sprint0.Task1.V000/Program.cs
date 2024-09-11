0283c061d48b6ea8c70d11c6b06733a3ab0e2206
Иван Кокоулин <Tyuiu.KokoulinIV@outlook.com>
2024-09-11 20:11:01 +0500

    Task1

---
+++ Tyuiu.KokoulinIV.Sprint0.Task1.V000/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KokoulinIV.Sprint0.Task1.V000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Tyuiu.KokoulinIV.Sprint0.Task1.V01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KokoulinIV.Sprint0.Task1.V01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет");
+            string name = "Игорь";
+            int age = 45;
+            Console.WriteLine("Здравствуйте, меня зовут " +  name   +   "мне"  +  age  +  "лет");
+            Console.ReadKey();
+        }
+    }
+}
