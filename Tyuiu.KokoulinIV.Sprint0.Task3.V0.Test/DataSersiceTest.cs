92064097d7ed766eb552312e61770202488b10c9
KokoulinIV <wolfff1309@yandex.ru>
2024-09-16 13:00:41 +0500

    Task3

---
+++ Tyuiu.KokoulinIV.Sprint0.Task3.V0.Test/DataSersiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KokoulinIV.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.KokoulinIV.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataSersiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5,5));
+        }
+    }
+}
---
+++ Tyuiu.KokoulinIV.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KokoulinIV.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KokoulinIV.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b238b915-5846-40f5-abe6-2017f122b8fb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
