a7503ee7c64bbecae6566bf136baafc869669342
KokoulinIV <wolfff1309@yandex.ru>
2024-09-18 17:45:21 +0500

    Task6

---
+++ Tyuiu.KokoulinIV.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KokoulinIV.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static int AdditionArray(int[] number)
+            
+        {
+            var total = 0;
+            for (var i = 0; i < number.Length; i++)
+            {
+                total = total + number[i];
+            }
+            return total;
+        }
+        
+        public static int SubtractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+
+            while (index < numbers.Length)
+            {
+                total = total - numbers[index];
+                index++;
+            }
+            return total;
+        }
+        public static int MultiplicationArray(int[] number)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * number[index];
+                index++;
+            }
+            while (index < number.Length);
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.KokoulinIV.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KokoulinIV.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KokoulinIV.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06897365-cd9d-41b3-997e-e33f6c0aadeb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
